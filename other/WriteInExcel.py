44363eef26172bb119eef852da71328a62fbe456
656021898 <656021898@qq.com>
2018-09-07 14:18:38 +0800

    更新other

--- other/WriteInExcel.py
+++ other/WriteInExcel.py
@@ -2,17 +2,33 @@
 
 #往Excel内写入数据
 import xlwt
+import xlrd
 
-excelname = xlwt.
-sheet = excelname.add_sheet('循环添加')
-    r'E:\jmeter\apache-jmeter-4.0\bin\sosho_test\用户登录MW并加入社群\写入数据.xlsx'
-#写入文件路径
-i=1
-while i<5 :
-    name = 'name'+str(i)
-    mobile = 18500000000+i
-    email = str(mobile)+'@qq.com'
-    excelname.write(name,mobile,email)
-excelname.save()
+#读
+# # excelname = xlwt.Workbook(encoding='utf-8')
+# excelname = xlrd.open_workbook(r'E:\jmeter\apache-jmeter-4.0\bin\sosho_test\用户登录MW并加入社群\写入数据.xls')
+# #xlrd用来读取excel，URL=https://xlrd.readthedocs.io/en/latest/api.html
+# sheet = excelname.sheets()[0]
+# #sheets()用来获取sheet列表
+# print(sheet)
+
+
+#写
+excelname = xlwt.Workbook()
+sheet = excelname.add_sheet('写入的数据')
+i = 1
+while i<5:
+    mobile = 18500000000 + i
+    if i % 2 == 1:
+        sheet.write(i,0,('name'+str(i)))
+        sheet.write(i,1,mobile)
+        sheet.write(i,2,(str(mobile) + '@qq.com'))
+    else:
+        sheet.write(i,0,('name'+str(i-1)))
+        sheet.write(i,1,mobile)
+        sheet.write(i,2,(str(mobile-1) + '@qq.com'))
+    i += 1
+excelname.save('E:\\jmeter\\apache-jmeter-4.0\\bin\\sosho_test\\用户登录MW并加入社群\\写入数据1.xls')
+print('结束')
 
 
