e4172ed7787397c549ce5fc58c505ebdbeaf7c14
656021898 <656021898@qq.com>
2018-09-11 10:34:39 +0800

    更新deleteCommunityMember.py

--- deleteCommunityMember.py
+++ deleteCommunityMember.py
@@ -20,24 +20,29 @@ def delete_contact(mobile,password,com_id):
     request_url='http://test.admin.sosho.cn/server/api/contacts_manage/contacts'
     contact_list = json.loads(requests.post(url=request_url,data=form_data,headers=header).text)
         #requests.post(url=request_url,data=form_data,headers=header返回的结果是requests.models.Response，先把他的格式转成text，再解析
-    request_lists = contact_list['data']['items']
     num = contact_list['data']['counts']['total_items']
-        #将contact_id全部写入列表中
-    id_lists=[]
-    for request_list in request_lists:
-        id_lists.append(request_list['_id'])
 
-    #调用删除接口并删除数据
-    i=0
-    while num != 0:
+    while num > 0:  #开始循环获取contact_id并删除数据
+        contact_list = json.loads(requests.post(url=request_url, data=form_data, headers=header).text)#重新获取联系人列表
+        request_lists = contact_list['data']['items']#获取第一页items
+        #将contact_id全部写入列表中
+        id_lists=[]
+        for request_list in request_lists:
+            id_lists.append(request_list['_id'])
+        #调用删除接口并删除数据
+        i=0
         while i<len(id_lists):
             delete_url='http://test.admin.sosho.cn/server/api/contacts_manage/contacts/delete'
             form = {'cid':com_id,'contacts_ids':id_lists[i]}
             requests.post(url=delete_url,data=form,headers=header)
             i += 1
+        del request_lists[:]#清空第一页items
+        num = contact_list['data']['counts']['total_items']#重置num继续循环
 
     print('删除了%d条'%(len(id_lists)))
 
 
+
+#调用函数并删除数据
 delete_contact('18544444401','111111',2038)
 
